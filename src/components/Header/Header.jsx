5bfc520fb2b9c01d3a56bab4602bbace2b7f3339
DioBastard <polina.karpova.2004lao@gmail.com>
2024-03-05 10:00:46 +0300

    new

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
-import { useTelegram } from "../../hooks/useTelegtam";
+import { useTelegram } from "../hooks/useTelegtam";
 import './Header.css'
 
 
